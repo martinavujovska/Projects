c724277c0de53dc2ba07f5a81fdaf4577f16f9d2
martinavujovska <95173501+martinavujovska@users.noreply.github.com>
2022-12-21 16:49:37 +0100

    updated

---
+++ layout/app.js
@@ -0,0 +1,32 @@
+arr = ['about', 'services', 'clients', 'contacts'];
+
+for(let i=0;i<arr.length;i++) {
+    if(arr[i] === "about"){
+        if(document.getElementById(arr[i])) {
+            if(document.getElementById(arr[i]).style.display === "none" || document.getElementById(arr[i]).style.display === ""){
+                document.getElementById(arr[i]).style.display = "block"
+            }
+        }
+    }
+    else {
+        if(document.getElementById(arr[i])) {
+            document.getElementById(arr[i]).style.display = "none";
+        }
+    }
+}
+function showHide(value) {
+    for(let i=0;i<arr.length;i++) {
+        if(arr[i] === value){
+            if(document.getElementById(arr[i])) {
+                if(document.getElementById(arr[i]).style.display === "none"){
+                    document.getElementById(arr[i]).style.display = "block"
+                }
+            }
+        }
+        else {
+            if(document.getElementById(arr[i])) {
+                document.getElementById(arr[i]).style.display = "none";
+            }
+        }
+    }
+}

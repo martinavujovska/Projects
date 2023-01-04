c001c32b6af451dd81edd6d3276ee3dfea8cddd0
martinavujovska <95173501+martinavujovska@users.noreply.github.com>
2023-01-04 16:21:48 +0100

    Add files via upload

---
+++ functions/app.js
@@ -0,0 +1,49 @@
+// let numbers = [1, 5, 7, 4, 3];
+// let sortedNumbers = [];
+
+// while (numbers.length > 0) {
+//     let smallestNumber = numbers[0];
+//     for (let i = 1; i < numbers.length; i++) {
+//         if (numbers[i] < smallestNumber) {
+//             smallestNumber = numbers[i];
+//         }
+//     }
+
+//     numbers.splice(numbers.indexOf(smallestNumber), 1);
+
+//     sortedNumbers.push(smallestNumber);
+// }
+
+// console.log(sortedNumbers); 
+
+///////////////////////////////////////////////////////////////////////////////
+// let a = 3;
+// let b = 5;
+// let c;
+
+// c = a; // 3
+// a = b;// 5
+// b = c; //3
+
+// console.log(a);
+// console.log(b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

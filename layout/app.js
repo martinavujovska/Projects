b540cc91fb8ee3e0635c8272f2a7e920ddbe5513
martinavujovska <95173501+martinavujovska@users.noreply.github.com>
2023-01-12 15:53:02 +0100

    Add files via upload

--- layout/app.js
+++ layout/app.js
@@ -1,40 +1,33 @@
 const burgerMenu = document.querySelector("#burgerMenu");
 const sideNav = document.querySelector("#sideNav");
+const article = document.querySelector(".article")
+
 
 arr = ['about', 'services', 'clients', 'contacts'];
 
-for(let i=0;i<arr.length;i++) {
-    if(arr[i] === "about"){
-        if(document.getElementById(arr[i])) {
-            if(document.getElementById(arr[i]).style.display === "none" || document.getElementById(arr[i]).style.display === ""){
-                document.getElementById(arr[i]).style.display = "block"
-            }
-        }
-    }
-    else {
-        if(document.getElementById(arr[i])) {
-            document.getElementById(arr[i]).style.display = "none";
-        }
-    }
-}
+showHide("about");
+
 function showHide(value) {
-    for(let i=0;i<arr.length;i++) {
-        if(arr[i] === value){
-            if(document.getElementById(arr[i])) {
-                if(document.getElementById(arr[i]).style.display === "none"){
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === value) {
+            if (document.getElementById(arr[i])) {
+                if (document.getElementById(arr[i]).style.display === "none") {
                     document.getElementById(arr[i]).style.display = "block"
                 }
             }
         }
         else {
-            if(document.getElementById(arr[i])) {
+            if (document.getElementById(arr[i])) {
                 document.getElementById(arr[i]).style.display = "none";
             }
         }
     }
 }
 
-burgerMenu.addEventListener("click", function(){
+burgerMenu.addEventListener("click", function () {
     sideNav.classList.toggle("sidenav-active");
-    // document.querySelector(".article")
+    article.classList.toggle("article-push");
 });
+
+
+
